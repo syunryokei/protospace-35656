df2571509ab6574ab3b248fff3c1cdfaca0b0869
syunryokei <77194027+syunryokei@users.noreply.github.com>
2021-07-21 22:51:21 +0900

    ユーザー登録のバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :user_name, presence: true
-         validates :profile, presence: true
+          with_options presence: true do
+            validates :user_name 
+            validates :profile 
+            validates :occupation 
+            validates :position 
+          end
 
          has_many :prototypes
          has_many :comments
